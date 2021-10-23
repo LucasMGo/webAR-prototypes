d7384b9fc3284101b09a6bf742695412028280ea
Lucas <44418036+LucasMGo@users.noreply.github.com>
2021-10-23 16:40:01 +0200

    add content to home, about pages

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,27 @@
 <template>
-  <div>
-    <h1>This is the About page</h1>
-  </div>
+  <v-container class="ma-md-12">
+    <v-row
+      ><v-col cols="12"><h1 class="font-weight-light">About</h1></v-col></v-row
+    >
+    <v-row>
+      <v-col cols="12" lg="4" class="font-weight-light">
+        <p>
+          Die Entwicklung des WebAR Onlineshops erfolgte im Rahmen einer
+          Bachelorarbeit zur Evaluation und Identifizierung von Potenzialen von
+          Frameworks im Bereich der webbasierten Augmented Reality. Es handelt
+          sich hierbei lediglich um einen Prototypen zu Testzwecken.
+        </p>
+        <p>
+          Die 3D Modelle der Produkte stammen von Nutzern der Plattform
+          <a href="https://sketchfab.com/">Sketchfab</a>. Die genutzten Modelle
+          wurden unter Creative Commons Lizenzen veröffentlicht und können daher
+          im Rahmen der Bachelorarbeit genutzt werden. Eine Erwähnung der
+          Ersteller befindet sich direkt unter den entsprechenden Modellen auf
+          den Produktseiten.
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
-    <h1>This is the Home page</h1>
-  </div>
+  <v-container class="ma-md-12">
+    <v-row
+      ><v-col cols="12"><h1 class="font-weight-light">Home</h1></v-col></v-row
+    >
+    <v-row>
+      <v-col cols="12" lg="4" class="font-weight-light">
+        <p>
+          Willkommen im WebAR Onlineshop. Hier stehen mehrere Produkte zur
+          Auswahl, die mittels Augmented Reality in ihren Räumlichkeiten
+          angezeigt werden können.
+        </p>
+        <p>
+          Es handelt sich hierbei um einen Prototypen zur Evalution und
+          Identifizierung von Potenzialen von Frameworks im Bereich der
+          webbasierten Augmented Reality.
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
